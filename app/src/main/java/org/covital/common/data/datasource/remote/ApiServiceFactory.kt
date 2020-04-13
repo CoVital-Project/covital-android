9eea60f715bf83db3960818ea47f0bf29ff0b4d4
Jason Pearson <jason.d.pearson@gmail.com>
2020-04-13 09:12:55 -0400

    #29 made okhttp init lazy (#44)

--- app/src/main/java/org/covital/common/data/datasource/remote/ApiServiceFactory.kt
+++ app/src/main/java/org/covital/common/data/datasource/remote/ApiServiceFactory.kt
@@ -1,5 +1,7 @@
 package org.covital.common.data.datasource.remote
 
+import android.content.Context
+import okhttp3.*
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,14 +14,25 @@ object ApiServiceFactory {
     // Update this url with the real backend
     private const val BASE_URL = "https://api.github.com"
 
-    fun create(moshi: Moshi): ApiService {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.BODY }
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+    const val OKHTTP_CACHE_SIZE = 10L * 1024 * 1024
+
+    fun create(context: Context, moshi: Moshi): ApiService {
+
+        val okhttp: OkHttpClient by lazy {
+            OkHttpClient.Builder()
+                .cache(Cache(context.cacheDir, OKHTTP_CACHE_SIZE))
+                .addInterceptor(HttpLoggingInterceptor()
+                    .apply { level = HttpLoggingInterceptor.Level.BODY })
+                .build()
+        }
 
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)
+            .callFactory(object: Call.Factory {
+                override fun newCall(request: Request): Call {
+                    return okhttp.newCall(request)
+                }
+            })
             .addConverterFactory(EmptyResponseFactory())
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
